8302a6b6103454e56324a0fab4ebc97b67be8962
Gus Esquivel <gesquive@gmail.com>
2016-07-28 08:30:26 -0500

    add server code for testing purposes

---
+++ server/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	// "bufio"
+	"fmt"
+	"net"
+	"os"
+	// "strings"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	// Listen for incoming connections.
+	ln, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer ln.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		// Listen for an incoming connection.
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	for {
+		reqLen, err := conn.Read(buf)
+		if reqLen == 0 {
+			return
+		}
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			return
+		}
+		fmt.Printf("%3d>> %s\n", reqLen, buf[0:reqLen])
+	}
+
+	// Read the incoming connection into the buffer.
+	// reqLen, err := conn.Read(buf)
+	// if err != nil {
+	// 	fmt.Println("Error reading:", err.Error())
+	// }
+	// // Send a response back to person contacting us.
+	// fmt.Printf(">> %s\n", buf[0:reqLen])
+	// Close the connection when you're done with it.
+	// conn.Close()
+	//
+	// // will listen for message to process ending in newline (\n)
+	// message, err := bufio.NewReader(conn).ReadString('\n')
+	// if err != nil {
+	// 	fmt.Printf("%v", err)
+	// 	return
+	// }
+	// if len(message) > 0 {
+	// 	// output message received
+	// 	fmt.Print("Message Received:", string(message))
+	// 	// sample process for string received
+	// 	newmessage := strings.ToUpper(message)
+	// 	// send new string back to client
+	// 	conn.Write([]byte(newmessage + "\n"))
+	// }
+}
+
+//
+// func main() {
+//
+// 	fmt.Println("Launching server...")
+//
+// 	// listen on all interfaces
+// 	ln, _ := net.Listen("tcp", ":8081")
+//
+// 	// accept connection on port
+// 	conn, _ := ln.Accept()
+//
+// 	// run loop forever (or until ctrl-c)
+// 	for {
+// 		// will listen for message to process ending in newline (\n)
+// 		message, err := bufio.NewReader(conn).ReadString('\n')
+// 		if err != nil {
+// 			fmt.Printf("%v", err)
+// 		}
+// 		if len(message) > 0 {
+// 			// output message received
+// 			fmt.Print("Message Received:", string(message))
+// 			// sample process for string received
+// 			newmessage := strings.ToUpper(message)
+// 			// send new string back to client
+// 			conn.Write([]byte(newmessage + "\n"))
+//
+// 		}
+// 	}
+// }
