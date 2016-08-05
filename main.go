4339d7c4088805ef62c32bcfc9c0e34fe4d2e660
Gus Esquivel <gesquive@gmail.com>
2016-08-05 17:52:01 -0500

    fix app version output

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "os"
 import "fmt"
 import "path/filepath"
 
-var version = "0.1.0"
+var version = "v0.1.0"
 var dirty = ""
 
 func main() {
-	displayVersion := fmt.Sprintf("%s v%s%s",
+	displayVersion := fmt.Sprintf("%s %s%s",
 		filepath.Base(os.Args[0]),
 		version,
 		dirty)
